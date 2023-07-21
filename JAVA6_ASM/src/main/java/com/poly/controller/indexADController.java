d0e2466cd94cfda85fceed6b79e33165089862e0
VinxVin069 <trantrungvinh.ctst@gmail.com>
2023-07-21 23:43:02 +0700

    FE- Vinh da fix QL Thong KE

--- JAVA6_ASM/src/main/java/com/poly/controller/indexADController.java
+++ JAVA6_ASM/src/main/java/com/poly/controller/indexADController.java
@@ -2,6 +2,7 @@ package com.poly.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,5 +17,15 @@ public class indexADController {
 	@RequestMapping("/quanLySanPham")
 	public String quanLySanPham(Model model) {
 		return "/homeAD/quanLySanPham";
+	}
+	
+	@GetMapping("/thongke/form")
+	public String quanLyThongKe(Model model) {
+		return "/homeAD/QLThongKe";
+	}
+	
+	@GetMapping("/thongke/form2")
+	public String quanLyThongKe2(Model model) {
+		return "/homeAD/QLThongKe2";
 	}
 }
