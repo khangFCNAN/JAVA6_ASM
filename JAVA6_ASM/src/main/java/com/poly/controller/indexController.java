4b97f65707925c9f67013e16d156f0a802e4766e
camnghi <camnghi07@gmail.com>
2023-07-21 22:15:41 +0700

    FEGioHangDonHangDatHang

--- JAVA6_ASM/src/main/java/com/poly/controller/indexController.java
+++ JAVA6_ASM/src/main/java/com/poly/controller/indexController.java
@@ -1,15 +1,8 @@
 package com.poly.controller;
 
-import java.text.DecimalFormat;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/index")
@@ -17,16 +10,25 @@ public class indexController {
 	
 	@RequestMapping("/form")
 	public String index(Model model) {
-		return "home/index";
+		return "/home/index";
 	}
 	
 	@RequestMapping("/sanpham")
 	public String sanpham(Model model) {
-		return "home/sanpham";
+		return "/home/sanpham";
 	}
 	
 	@RequestMapping("/giohang")
 	public String giohang(Model model) {
-		return "home/giohang";
+		return "/home/giohang";
+	}
+	@RequestMapping("/dathang")
+	public String dathang(Model model) {
+		return "/home/dathang";
+	}
+	
+	@RequestMapping("/donhang")
+	public String donhang(Model model) {
+		return "/home/donhang";
 	}
 }
