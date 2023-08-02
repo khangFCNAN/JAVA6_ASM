3648a207f492c7c6d80e56cdede6af582d185566
trongphuc0811 <134265173+trongphuc0811@users.noreply.github.com>
2023-08-02 22:40:27 +0700

    đã làm đổ dự liệu bên an khang

---
+++ JAVA6_ASM/src/main/java/com/poly/controller/indeWebController.java
@@ -0,0 +1,74 @@
+package com.poly.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import com.poly.entity.SanPham;
+
+@CrossOrigin("*")
+@Controller
+@RequestMapping("/index")
+public class indeWebController {
+	
+	 @Autowired
+	 private RestTemplate restTemplate;
+
+	@RequestMapping("/form")
+	public String index(Model model) {
+		ResponseEntity<SanPham[]> reponse = restTemplate.getForEntity("http://localhost:8080/api/sanpham", SanPham[].class);
+		SanPham[] sanphams = reponse.getBody();
+		model.addAttribute("sanphams",sanphams);
+		return "/home/index";
+	}
+
+	@RequestMapping("/sanpham")
+	public String sanpham(Model model) {
+		return "home/sanpham";
+	}
+	@RequestMapping("/chitietsanpham")
+	public String chitietsanpham(Model model) {
+		return "/chitietsanpham";
+	}
+	@RequestMapping("/giohang")
+	public String giohang(Model model) {
+		return "/home/giohang";
+	}
+	@RequestMapping("/dathang")
+	public String dathang(Model model) {
+		return "/home/dathang";
+	}
+	
+	@RequestMapping("/donhang")
+	public String donhang(Model model) {
+		return "/home/donhang";
+	}
+	
+	@RequestMapping("/DangNhap")
+	public String DangNhap(Model model) {
+		return "/home/DangNhap";
+	}
+	
+	@RequestMapping("/DangKy")
+	public String DangKy(Model model) {
+		return "/home/DangKy";
+	}
+	
+	@RequestMapping("/DoiMatKhau")
+	public String DoiMatKhau(Model model) {
+		return "/home/DoiMatKhau";
+	}
+	
+	@RequestMapping("/CapNhatTaiKhoan")
+	public String CapNhatTaiKhoan(Model model) {
+		return "/home/CapNhatTaiKhoan";
+	}
+	@RequestMapping("/QuenMatKhau")
+	public String QuenMatKhau(Model model) {
+		return "/home/QuenMatKhau";
+	}
+}
--- JAVA6_ASM/src/main/java/com/poly/controller/indexController.java
+++ JAVA6_ASM/src/main/java/com/poly/controller/indexController.java
@@ -1,62 +1,25 @@
 package com.poly.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.poly.dao.SanphamDAO;
+import com.poly.entity.SanPham;
 
-@Controller
-@RequestMapping("/index")
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/sanpham")
 public class indexController {
-
-	@RequestMapping("/form")
-	public String index(Model model) {
-		return "/home/index";
-	}
-
-	@RequestMapping("/sanpham")
-	public String sanpham(Model model) {
-		return "home/sanpham";
-	}
-	@RequestMapping("/chitietsanpham")
-	public String chitietsanpham(Model model) {
-		return "/chitietsanpham";
-	}
-	@RequestMapping("/giohang")
-	public String giohang(Model model) {
-		return "/home/giohang";
-	}
-	@RequestMapping("/dathang")
-	public String dathang(Model model) {
-		return "/home/dathang";
-	}
+	@Autowired
+	private SanphamDAO sanphamdao;
 	
-	@RequestMapping("/donhang")
-	public String donhang(Model model) {
-		return "/home/donhang";
-	}
-	
-	@RequestMapping("/DangNhap")
-	public String DangNhap(Model model) {
-		return "/home/DangNhap";
-	}
-	
-	@RequestMapping("/DangKy")
-	public String DangKy(Model model) {
-		return "/home/DangKy";
-	}
-	
-	@RequestMapping("/DoiMatKhau")
-	public String DoiMatKhau(Model model) {
-		return "/home/DoiMatKhau";
-	}
-	
-	@RequestMapping("/CapNhatTaiKhoan")
-	public String CapNhatTaiKhoan(Model model) {
-		return "/home/CapNhatTaiKhoan";
-	}
-	@RequestMapping("/QuenMatKhau")
-	public String QuenMatKhau(Model model) {
-		return "/home/QuenMatKhau";
+	@GetMapping
+	public List<SanPham> getSanPhams(){
+		return sanphamdao.findAll();
 	}
 }
