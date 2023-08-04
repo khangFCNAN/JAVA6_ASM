4ca33264f0b6cffc38228fee05c8baeadeaff213
trongphuc0811 <134265173+trongphuc0811@users.noreply.github.com>
2023-08-04 16:00:40 +0700

    API hdon hang

---
+++ JAVA6_ASM/src/main/java/com/poly/RestAPIController/DonHangRestAPIController.java
@@ -0,0 +1,28 @@
+package com.poly.RestAPIController;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.poly.dao.HoadonDAO;
+import com.poly.entity.HoaDon;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/list")
+public class DonHangRestAPIController {
+
+	@Autowired
+	public HoadonDAO hdDao;
+
+	@GetMapping("/hoadon")
+	public List<HoaDon> getAllHoaDons(Model model) {
+		return hdDao.findAll();
+
+	}
+}
