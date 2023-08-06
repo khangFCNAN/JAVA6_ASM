b4f72c5e9b90a9473c9654da5d0f70d1c4006b3f
NAK-203 <akhangnguyen203@gmail.com>
2023-08-06 17:17:26 +0700

    xong

--- JAVA6_ASM/src/main/java/com/poly/controller/SanPhamWebController.java
+++ JAVA6_ASM/src/main/java/com/poly/controller/SanPhamWebController.java
@@ -1,6 +1,7 @@
 package com.poly.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.poly.entity.SanPham;
+import com.poly.service.SanPhamService;
+
 @Controller
 @RequestMapping("/quanLySanPham")
 public class SanPhamWebController {
 	 
-	 @Autowired	 
+	@Autowired
+	 private SanPhamService spservice;
+	 
 	 @GetMapping("/list")
-	    public String listSanPham(Model model) throws IllegalStateException, IOException  {
-	       return "/homeAD/quanLySanPham";
-	 }
+	    public String listSanPham(Model model) {
+	       List<SanPham> listSp = spservice.findAll();
+	       model.addAttribute("sanphams", listSp);
+	        return "homeAD/quanLySanPham";
+	  }
 	 
 
 }
